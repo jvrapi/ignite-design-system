fc2eb352199054c773dee8a70ffa8e73edc81a0e
Joao Lima <joaooviitorr@hotmail.com>
2024-09-21 11:28:37 -0300

    feat: added missing tokens docs

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -4,7 +4,7 @@ import { Button, ButtonProps } from '@jvrapi-ignite-ui/react';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
+  title: 'Form/Button',
   component: Button,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
